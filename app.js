fe81431e4f8f23e612d578ebc2b84f56f2c37b9b
Patryk Gmórski <patryk121345@gmail.com>
2017-10-28 14:09:51 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+function odliczanie()
+		{
+
+			var dzisiaj = new Date();
+			// podgrupy -->
+			var godzina = dzisiaj.getHours();
+			var minuta = dzisiaj.getMinutes();
+			var sekunda = dzisiaj.getSeconds();
+			//0 na początku-->
+			if (minuta < 10)
+			minuta = '0' + minuta;
+			if (sekunda < 10)
+			sekunda = '0' + sekunda;
+			// dodanie zegara -->
+			document.getElementById("zegar").innerHTML =
+				godzina+":"+minuta+":"+sekunda;
+				// co 1 sec -->
+				setTimeout("odliczanie()",1000);
+
+
+		}
